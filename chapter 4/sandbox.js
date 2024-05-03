e74f7f7e6c7318372b7372cba37d4eac06ea1821
SumailKaur777 <156945133+SumailKaur777@users.noreply.github.com>
2024-05-03 11:48:19 -0500

    first commit

---
+++ chapter 4/sandbox.js
@@ -0,0 +1,57 @@
+//function expression
+
+/*const speak = function(){
+    console.log('good day!');
+};
+
+//speak();
+
+greet();
+greet();
+greet();
+
+//function declaration
+function greet(){
+    console.log('hello there');
+}
+// arguments and parameters
+const speak = function(name){
+    console.log(`good day ${name}`);
+};
+
+speak('luigi');
+
+
+
+//returning values
+
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`good ${time} ${name}`);
+};
+
+
+//regular function
+const calcArea = function(radius){
+    return 3.14 * radius**2;
+    
+}
+const area = calcArea(2);
+console.log(area);
+
+//arrow function
+const calcArea = (radius) => {
+    return 3.14 * radius**2;
+    
+};
+const area = calcArea(2);
+console.log('area:', area);
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i]* tax;
+    }
+    return total;
+};
+
+console.log(bill([10,15,30], 0.2));*/
